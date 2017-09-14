e60a113375507a088644e1c50f1575193817fbae
daliannyvieira <dalianny-sb@hotmail.com>
2017-09-14 17:50:55 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
-var logo = anime({
-  targets: '.logo',
-  translateY: {
-    value: ['-4vh', 0],
-    duration: 800
-  },
-  rotate: {
-    value: 360,
-    duration: 1800,
-    easing: 'easeInOutSine'
-  },
-  delay: 250
-});
+// var logo = anime({
+//   targets: '.logo-img-1',
+//   translateY: {
+//     value: ['-4vh', 0],
+//     duration: 800
+//   },
+//   rotate: {
+//     value: 360,
+//     duration: 1800,
+//     easing: 'easeInOutSine'
+//   },
+//   delay: 250
+// });
+
+var basicTimeline = anime.timeline();
+
+basicTimeline
+  .add({
+    targets: '.logo-img-2',
+    translateX: -100,
+    easing: 'easeOutExpo',
+    delay: 1250
+  })
+  .add({
+    targets: '.logo-img-3',
+    translateX: -200,
+    easing: 'easeOutExpo',
+    rotate: 360
+  })
+  .add({
+    targets: '.logo-img-4',
+    translateX: -300,
+    easing: 'easeOutExpo',
+    rotate: 360
+  })
+  .add({
+    targets: '.logo-img-5',
+    translateX: -400,
+    easing: 'easeOutExpo',
+    rotate: 360
+  });
 
 var purpose = anime({
  targets: '.oi',
@@ -18,5 +46,5 @@ var purpose = anime({
    value: ['4vh', 0],
    duration: 800
  },
- delay: 2250
+ delay: 500
 });
