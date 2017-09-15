450678ff4bfab195fde0580ccfcbb357ea4990f0
daliannyvieira <dalianny-sb@hotmail.com>
2017-09-14 23:21:47 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,38 +1,3 @@
-var basicTimeline = anime.timeline();
-
-basicTimeline
-  .add({
-    targets: '.logo',
-    translateY: {
-     value: ['-2.5vh', 0],
-     duration: 1200
-    },
-    delay: 250
-  })
-  .add({
-    targets: '.logo-img-2',
-    translateX: -100,
-    easing: 'easeOutExpo',
-  })
-  .add({
-    targets: '.logo-img-3',
-    translateX: -200,
-    easing: 'easeOutExpo',
-    rotate: 360
-  })
-  .add({
-    targets: '.logo-img-4',
-    translateX: -300,
-    easing: 'easeOutExpo',
-    rotate: 360
-  })
-  .add({
-    targets: '.logo-img-5',
-    translateX: -400,
-    easing: 'easeOutExpo',
-    rotate: 360
-  });
-
 var purpose = anime({
  targets: '.oi',
  translateY: {
@@ -41,3 +6,63 @@ var purpose = anime({
  },
  delay: 5000
 });
+
+var wrap = document.querySelector('.intro-img');
+
+document.body.addEventListener('mousemove', function (event) {
+  var halfWidth = window.innerWidth / 2;
+  var halfHeight = window.innerHeight / 2;
+  var dX = -(halfWidth - event.clientX) * 0.015;
+  var dY = (halfHeight - event.clientY) * 0.015;
+
+  wrap.style.transform = 'rotateX(' + dY + 'deg) rotateY(' + dX + 'deg)';
+});
+
+var startAnimation = function(event){
+
+  event.preventDefault();
+
+  console.log('oi')
+
+  var basicTimeline = anime.timeline();
+
+  basicTimeline
+    .add({
+      targets: '.logo',
+      translateY: {
+       value: ['-2.5vh', 0],
+       duration: 1200
+      },
+      delay: 250
+    })
+    .add({
+      targets: '.logo-img-2',
+      translateX: -100,
+      easing: 'easeOutExpo',
+    })
+    .add({
+      targets: '.logo-img-3',
+      translateX: -200,
+      easing: 'easeOutExpo',
+      rotate: 360
+    })
+    .add({
+      targets: '.logo-img-4',
+      translateX: -300,
+      easing: 'easeOutExpo',
+      rotate: 360
+    })
+    .add({
+      targets: '.logo-img-5',
+      translateX: -400,
+      easing: 'easeOutExpo',
+      rotate: 360,
+      complete: function(anim) {
+        if (anim.completed) {
+          document.querySelector('.logo').removeEventListener('mouseenter', startAnimation)
+        }
+      }
+    });
+  }
+
+document.querySelector('.logo').addEventListener('mouseenter', startAnimation);
